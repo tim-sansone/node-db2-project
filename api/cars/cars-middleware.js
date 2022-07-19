0cbb27cf46735ca680aee8965ebf2b4470cc83e4
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-07-19 14:31:18 -0500

    get by id functional

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,17 @@
+const Cars = require('./cars-model')
+
 const checkCarId = (req, res, next) => {
-  // DO YOUR MAGIC
+  const { id } = req.params;
+  Cars.getById(id)
+    .then(car => {
+      if(!car){
+        next({status: 404, message: `car with id ${id} is not found`})
+      } else{
+        req.car = car
+        next()
+      }
+    })
+    .catch(next);
 }
 
 const checkCarPayload = (req, res, next) => {
@@ -12,4 +24,8 @@ const checkVinNumberValid = (req, res, next) => {
 
 const checkVinNumberUnique = (req, res, next) => {
   // DO YOUR MAGIC
+}
+
+module.exports = {
+  checkCarId
 }
--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,10 +1,16 @@
 const express = require('express');
 const router = express.Router();
 
+const { checkCarId } = require('./cars-middleware');
+
 const Cars = require('./cars-model')
 
 router.get('/', (req, res) => {
     Cars.getAll().then(cars => res.json(cars))
+})
+
+router.get('/:id', checkCarId, (req, res) => {
+    res.json(req.car);
 })
 
 module.exports = router;
