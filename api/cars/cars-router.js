8eccd57638c43686ab54fbb3a61486831b29b433
Tim Sansone <tim.sansone@Tim-Sansone.local>
2022-07-19 14:23:55 -0500

    get all endpoint functional

--- api/cars/cars-router.js
+++ api/cars/cars-router.js
@@ -1,1 +1,10 @@
-// DO YOUR MAGIC
+const express = require('express');
+const router = express.Router();
+
+const Cars = require('./cars-model')
+
+router.get('/', (req, res) => {
+    Cars.getAll().then(cars => res.json(cars))
+})
+
+module.exports = router;
--- api/server.js
+++ api/server.js
@@ -1,7 +1,13 @@
-const express = require("express")
+const express = require("express");
+const server = express();
 
-const server = express()
+const carsRouter = require('./cars/cars-router');
 
-// DO YOUR MAGIC
+server.use(express.json());
+server.use('/api/cars', carsRouter);
+
+server.use((error, req, res, next) => {
+    res.status(error.status || 500).json({message: error.message || 'internal server error'})
+})
 
 module.exports = server
